e8563950b643206629e3771792d820b8bb576196
kimnamlhn <kimnam.cpc@gmail.com>
2021-06-22 20:25:47 +0700

    lab00

---
+++ Lab00/App_Code/CommonPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace Lab00.App_Code
+{
+    public class CommonPage : System.Web.UI.Page
+    {
+        public Dictionary<string, string> UserList
+        {
+            get 
+        }
+    }
+}
